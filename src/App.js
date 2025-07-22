260df6ae8d69002b5a8f0e15b423661d373ae9b7
ShikshaBhardwaj8 <159452410+ShikshaBhardwaj8@users.noreply.github.com>
2025-07-22 20:31:33 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -123,7 +123,7 @@ function App() {
   return (
     <div className="app">
       <div className="app_nav">
-        <h1>🗂️ 𝕂𝔸ℕ𝔹𝔸ℕ 𝔹𝕆𝔸ℝ𝔻</h1>
+        <h1>🗂️『 𝑲𝑨𝑵𝑩𝑨𝑵 𝑩𝑶𝑨𝑹𝑫 』</h1>
       </div>
       <div className="app_boards_container">
         <div className="app_boards">
